95b8350c50d6d90fb3e5b3e32c07cf3011ce57ff
Abhishek Das <dabhishek31oct@gmail.com>
2022-07-23 22:03:56 +0530

    Updates

---
+++ precache-manifest.e70827208092292d571316fa107d0be3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "f890aa3582f06096ac5c",
+    "url": "/bloogin/static/css/main.1684bf51.chunk.css"
+  },
+  {
+    "revision": "f890aa3582f06096ac5c",
+    "url": "/bloogin/static/js/main.f890aa35.chunk.js"
+  },
+  {
+    "revision": "0cd57e100a6202208881",
+    "url": "/bloogin/static/js/1.0cd57e10.chunk.js"
+  },
+  {
+    "revision": "ff2493009ade6818267a",
+    "url": "/bloogin/static/js/2.ff249300.chunk.js"
+  },
+  {
+    "revision": "cdc005c5d8325ea9f8fc",
+    "url": "/bloogin/static/js/3.cdc005c5.chunk.js"
+  },
+  {
+    "revision": "b6328d7d07f8e8d84f5f",
+    "url": "/bloogin/static/js/4.b6328d7d.chunk.js"
+  },
+  {
+    "revision": "a9289f4f80278be00651",
+    "url": "/bloogin/static/js/runtime~main.a9289f4f.js"
+  },
+  {
+    "revision": "08109b8983786616849dc8db1f010c26",
+    "url": "/bloogin/static/media/bloggin_logo_light.08109b89.png"
+  },
+  {
+    "revision": "5fe1d1def1f036e076e7d283ea180d05",
+    "url": "/bloogin/static/media/bloggin_logo_dark.5fe1d1de.png"
+  },
+  {
+    "revision": "7988a319420dfa9121d2b662ade09c35",
+    "url": "/bloogin/static/media/bloggin_logo.7988a319.png"
+  },
+  {
+    "revision": "dc2d78114e9e743a3c75ee3408e70abb",
+    "url": "/bloogin/index.html"
+  }
+];
---
+++ static/js/2.ff249300.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{164:function(n,e,t){"use strict";t.r(e);var u=t(0),o=t.n(u);e.default=function(){return o.a.createElement("div",null,"About")}}}]);
+//# sourceMappingURL=2.ff249300.chunk.js.map
